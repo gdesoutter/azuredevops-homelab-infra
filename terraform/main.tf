779c3b54243dcd18ab44e43a753116b6ecd3cf94
gdesoutter <gdesoutter@outlook.com>
2026-02-14 15:40:03 +0100

    Déploiement DC-02

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-# --- 1. Création du Disque OS ---
 resource "null_resource" "os_disk" {
+  for_each = var.vm_catalog
+
   triggers = {
-    vm_name  = var.vm_name
+    vm_name  = each.key
     host     = var.hyperv_host
     user     = var.hyperv_user
     password = var.hyperv_password
@@ -19,25 +20,26 @@ resource "null_resource" "os_disk" {
 
   provisioner "remote-exec" {
     inline = [
-      "powershell.exe -ExecutionPolicy Bypass -Command \"New-VHD -Path 'C:\\Hyper-V\\VHDs\\${self.triggers.vm_name}.vhdx' -ParentPath 'C:\\Hyper-V\\Templates\\Server2025_Master.vhdx' -Differencing\""
+      "powershell.exe -ExecutionPolicy Bypass -Command \"if (-not (Test-Path 'C:\\Hyper-V\\VHDs\\${self.triggers.vm_name}.vhdx')) { New-VHD -Path 'C:\\Hyper-V\\VHDs\\${self.triggers.vm_name}.vhdx' -ParentPath 'C:\\Hyper-V\\Templates\\Server2025_Master.vhdx' -Differencing } else { Write-Host 'Le disque existe deja' }\""
     ]
   }
 
   provisioner "remote-exec" {
-    when    = destroy
+    when   = destroy
     inline = [
       "powershell.exe -ExecutionPolicy Bypass -Command \"Remove-Item -Path 'C:\\Hyper-V\\VHDs\\${self.triggers.vm_name}.vhdx' -Force -ErrorAction SilentlyContinue\""
     ]
   }
 }
 
-# --- 2. La Machine Virtuelle Hyper-V ---
 resource "hyperv_machine_instance" "vm" {
-  name            = var.vm_name
-  generation      = 2
-  processor_count = 2
-  static_memory   = true
-  memory_startup_bytes = 4294967296 
+  for_each = var.vm_catalog
+
+  name                 = each.key
+  generation           = 2
+  processor_count      = each.value.vcpu
+  static_memory        = true
+  memory_startup_bytes = each.value.ram_mb * 1024 * 1024
 
   depends_on = [null_resource.os_disk]
 
@@ -45,17 +47,17 @@ resource "hyperv_machine_instance" "vm" {
     controller_type     = "Scsi"
     controller_number   = 0
     controller_location = 0
-    path                = "C:\\Hyper-V\\VHDs\\${var.vm_name}.vhdx"
+    path                = "C:\\Hyper-V\\VHDs\\${each.key}.vhdx"
   }
 
   network_adaptors {
     name        = "eth0"
-    switch_name = "Lab-External" 
+    switch_name = each.value.switch_name
   }
 
   vm_firmware {
     enable_secure_boot   = "On"
-    secure_boot_template = "MicrosoftWindows" 
+    secure_boot_template = "MicrosoftWindows"
     boot_order {
       boot_type           = "HardDiskDrive"
       controller_number   = 0
@@ -64,24 +66,24 @@ resource "hyperv_machine_instance" "vm" {
   }
 }
 
-# --- 3. Provisioning (Onboarding Auto) ---
 resource "null_resource" "onboarding" {
+  for_each = var.vm_catalog
+
   triggers = {
-    vm_id       = hyperv_machine_instance.vm.id
+    vm_id       = hyperv_machine_instance.vm[each.key].id 
     script_hash = filebase64sha256("${path.module}/scripts/install_arc.ps1")
   }
 
   connection {
     type     = "winrm"
-    user     = "Administrateur"      
-    password = var.vm_admin_password    
-    host     = hyperv_machine_instance.vm.network_adaptors[0].ip_addresses[0]
+    user     = "Administrateur"
+    password = var.vm_admin_password
+    host     = hyperv_machine_instance.vm[each.key].network_adaptors[0].ip_addresses[0]
     https    = true
     insecure = true
-    timeout  = "15m" # On augmente à 15 min car Server 2025 peut être lent au premier boot
+    timeout  = "15m"
   }
 
-  # A. Préparation de l'environnement
   provisioner "remote-exec" {
     inline = [
       "powershell.exe -Command \"if (-not (Test-Path 'C:\\Temp')) { New-Item -ItemType Directory -Path 'C:\\Temp' -Force }\"",
@@ -89,16 +91,14 @@ resource "null_resource" "onboarding" {
     ]
   }
 
-  # B. Copie du script
   provisioner "file" {
     source      = "${path.module}/scripts/install_arc.ps1"
     destination = "C:/Temp/install_arc.ps1"
   }
 
-  # C. Exécution Automatique
   provisioner "remote-exec" {
     inline = [
-      "powershell.exe -ExecutionPolicy Bypass -File C:/Temp/install_arc.ps1 -TenantId \"${var.tenant_id}\" -ClientId \"${var.client_id}\" -ClientSecret \"${var.client_secret}\" -ResourceGroup \"${var.resource_group}\" -Location \"${var.location}\" -ResourceName \"${var.vm_name}\""
+      "powershell.exe -ExecutionPolicy Bypass -File C:/Temp/install_arc.ps1 -TenantId \"${var.tenant_id}\" -ClientId \"${var.client_id}\" -ClientSecret \"${var.client_secret}\" -ResourceGroup \"${var.resource_group}\" -Location \"${var.location}\" -ResourceName \"${each.key}\""
     ]
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,8 @@
-variable "hyperv_host" {
-  type = string
-}
-variable "hyperv_user" {
-  type      = string
-  sensitive = true
-}
-variable "hyperv_password" {
-  type      = string
-  sensitive = true
-}
+variable "hyperv_host" { type = string }
+variable "hyperv_user" { type = string; sensitive = true }
+variable "hyperv_password" { type = string; sensitive = true }
+variable "vm_admin_password" { type = string; sensitive = true }
 
-
-variable "vm_name" {
-  type    = string
-  default = "DC-02"
-}
-variable "vm_admin_password" {
-  type      = string
-  sensitive = true
-}
 
 variable "subscription_id" {}
 variable "tenant_id" {}
@@ -26,3 +10,25 @@ variable "client_id" {}
 variable "client_secret" { sensitive = true }
 variable "resource_group" {}
 variable "location" { default = "westeurope" }
+
+variable "vm_catalog" {
+  description = "Configuration de chaque VM à déployer"
+  type = map(object({
+    ram_mb      = number
+    vcpu        = number
+    switch_name = string
+  }))
+
+  default = {
+    "ARC-SRV-01" = {
+      ram_mb      = 4096
+      vcpu        = 2
+      switch_name = "Lab-External"
+    }
+    "DC-02" = {
+      ram_mb      = 8192
+      vcpu        = 2
+      switch_name = "Lab-External"
+    }
+  }
+}
